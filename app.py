35c60e092053610a0a880e055e31eb127e2f8777
claryssayuanwie <claryuanwie@gmail.com>
2025-07-30 18:55:51 -0400

    style

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on Vercel!"
+    return render_template('homepage.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001) 
